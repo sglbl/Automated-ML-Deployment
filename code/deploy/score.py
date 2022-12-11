d9a25a451c800f0e5cf199c6e689859d413a2221
Suleyman <silo1881@gmail.com>
2022-12-11 17:53:41 +0300

    score

--- code/deploy/score.py
+++ code/deploy/score.py
@@ -1,104 +1,22 @@
+import io
 import os
-import joblib
-import numpy as np
-import argparse
-
-from sklearn.svm import SVC
-from azureml.core import Model
-from azureml.monitoring import ModelDataCollector
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
-
-
-# # The init() method is called once, when the web service starts up.
-# # Typically you would deserialize the model file, as shown here using joblib,
-# # and store it in a global variable so your run() method can access it later.
-# def init():
-#     global model
-#     global inputs_dc, prediction_dc
-#     # The AZUREML_MODEL_DIR environment variable indicates
-#     # a directory containing the model file you registered.
-#     model_file_name = "model.pkl"
-#     model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
-#     model = joblib.load(model_path)
-#     inputs_dc = ModelDataCollector("sample-model", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
-#     prediction_dc = ModelDataCollector("sample-model", designation="predictions", feature_names=["prediction"])
-
-
-# # The run() method is called each time a request is made to the scoring API.
-# # Shown here are the optional input_schema and output_schema decorators
-# # from the inference-schema pip package. Using these decorators on your
-# # run() method parses and validates the incoming payload against
-# # the example input you provide here. This will also generate a Swagger
-# # API document for your web service.
-# @input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-# @output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
-# def run(data):
-#     # Use the model object loaded by init().
-#     result = model.predict(data)
-#     inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-#     prediction_dc.collect(result) #this call is saving our input data into Azure Blob
-
-#     # You can return any JSON-serializable object.
-#     return { "predict": result.tolist() }
-
-
-################################################
-
-
-
-# # The init() method is called once, when the web service starts up.
-# # Typically you would deserialize the model file, as shown here using joblib,
-# # and store it in a global variable so your run() method can access it later.
-# def init():
-#     global model
-#     global inputs_dc, prediction_dc
-#     # The AZUREML_MODEL_DIR environment variable indicates
-#     # a directory containing the model file you registered.
-#     # model_file_name = "saved_model.pb"
-#     # model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
-#     model_path = "../../models/age_model_a/saved_model.pb"
-#     model = joblib.load(model_path)
-#     inputs_dc = ModelDataCollector("age_model_a", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
-#     prediction_dc = ModelDataCollector("age_model_a", designation="predictions", feature_names=["prediction"])
-
-
-# # The run() method is called each time a request is made to the scoring API.
-# # Shown here are the optional input_schema and output_schema decorators
-# # from the inference-schema pip package. Using these decorators on your
-# # run() method parses and validates the incoming payload against
-# # the example input you provide here. This will also generate a Swagger
-# # API document for your web service.
-# @input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-# @output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
-# def run(data):
-#     # Use the model object loaded by init().
-#     result = model.predict(data)
-#     inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-#     prediction_dc.collect(result) #this call is saving our input data into Azure Blob
-
-#     # You can return any JSON-serializable object.
-#     return { "predict": result.tolist() }
-
-
-#############################################
-
 import json
 import numpy as np
 import tensorflow as tf
 import cv2 as cv
 from PIL import Image
 import requests
-#from inference_model import InferenceModel
- 
+import base64
 from azureml.contrib.services.aml_request import rawhttp
 from azureml.contrib.services.aml_response import AMLResponse
  
+# from django.core.files.base import ContentFile
 
 class InferenceModel():
     def __init__(self, model_path):
+        # Load the model from the path
         self.model = tf.keras.models.load_model(model_path)
+ 
  
     def face_border_detector(self, image):
         # download xml from server
@@ -117,68 +35,111 @@ class InferenceModel():
                 
         [x,y,w,h] = biggest_face
         cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # cv.imshow("image", image)
+        # cv.waitKey(0)
         return biggest_face
+
+
+    # Decode image from base64 to pillow and opencv images
+    def preprocess_image(self, image_encoded):     
+        pil_image = Image.open(io.BytesIO(base64.b64decode(image_encoded)))
+        pil_image_gray = pil_image.resize((48,48)).convert('L')
+        # pil_image_gray.show()
+        image_np = (255 - np.array(pil_image_gray.getdata())) / 255.0
+        cv_image = np.array(pil_image) # converting PIL image to cv2 image
+        cv_image = cv_image[:, :, ::-1].copy()  # convert from RGB to BGR
+
+        return cv_image, image_np.reshape(-1,48,48,1)
  
- 
-    def _preprocess_image(self, image_bytes):
-        image = Image.open(image_bytes)        
-        image = image.resize((48,48)).convert('L')
-        # image.show()
-        image_np = (255 - np.array(image.getdata())) / 255.0
- 
-        return image_np.reshape(-1,48,48,1)
  
     def predict(self, image_bytes):
-        image_data = self._preprocess_image(image_bytes)
+        opencv_image, image_data = self.preprocess_image(image_bytes)
         prediction = self.model.predict(image_data)
-        
-        face_borders = self.face_border_detector(cv.imread(image_bytes))
-        # print("Prediction: ", prediction)
-        # print("Face borders: ", face_borders)
-        face_borders.append(prediction)
-        # return [prediction, face_borders]
-        return face_borders
+        # print(prediction.tolist()[0][0], "Predict list")
+        predicted_age = int(prediction.tolist()[0][0])
+        face_borders_and_age = self.face_border_detector( opencv_image )
+        face_borders_and_age.append(predicted_age)
+        return face_borders_and_age
 
 
+def int32_to_int(obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
 
-# The init() method is called once, when the web service starts up.
-# Typically you would deserialize the model file, as shown here using joblib,
-# and store it in a global variable so your run() method can access it later.
+
 def init():
     global model
-    global inputs_dc, prediction_dc
-    # The AZUREML_MODEL_DIR environment variable indicates
-    # a directory containing the model file you registered.
-    # model_file_name = "saved_model.pb"
-    # model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
-    model_path = "../../models/age_model_a/saved_model.pb"
-    # model = joblib.load(model_path)
+    model_name = "age_model_a"
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), model_name)
     model = InferenceModel(model_path)
 
 
-# The run() method is called each time a request is made to the scoring API.
-# Shown here are the optional input_schema and output_schema decorators
-# from the inference-schema pip package. Using these decorators on your
-# run() method parses and validates the incoming payload against
-# the example input you provide here. This will also generate a Swagger
-# API document for your web service.
-# @input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-# @output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
-# def run(data):
-#     # Use the model object loaded by init().
-#     result = model.predict(data)
-#     inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-#     prediction_dc.collect(result) #this call is saving our input data into Azure Blob
-
-#     # You can return any JSON-serializable object.
-#     return { "predict": result.tolist() }
 @rawhttp
 def run(request):
     if request.method != 'POST':
         return AMLResponse(f"Unsupported verb: {request.method}", 400)
- 
-    image_data = request.files['image']
-    preds = model.predict(image_data)
+  
+    exc_occured = False
+    try:
+        file_bytes = request.get_data(False)    
+        preds = model.predict(file_bytes)
+    except Exception as e:
+        print("Exception occured in get_data(): ", e)
+        exc_occured = True        
     
-    return AMLResponse(json.dumps({"preds": preds.tolist()}), 200)
+    if exc_occured:
+        try:
+            file_bytes = request.get_json(force=True)
+            image_data = file_bytes["image"]
+            preds = model.predict(image_data)
+            exc_occured = False
+        except Exception as e:
+            print("Exception occured in get_json(): ", e)
+            exc_occured = True
     
+    if exc_occured:
+        try:
+            file_bytes = request.files['image'].read()
+            preds = model.predict(file_bytes)
+            exc_occured = False
+        except Exception as e:
+            print("Exception occured in files['image'].read(): ", e)
+            exc_occured = True
+    
+    if exc_occured:
+        try:
+            image_data = file_bytes
+            preds = model.predict(image_data)
+        except Exception as e:
+            print("Exception occured in image_data=file_btyes: ", e)
+            exc_occured = True
+    
+    # preds = model.predict(image_data)
+    dumped_preds = json.dumps({"preds": preds}, default=int32_to_int)
+    return AMLResponse(dumped_preds, 200)
+    
+
+# def encoder(self, image_src):
+#     # with open(image_src, "rb") as image_file:
+#     with open(image_src, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#     return encoded_string
+
+
+# def init_local():
+#     global model
+#     model_path = "../age_model_a"
+#     model = InferenceModel(model_path)
+    
+#     # encoded_photo = model.encoder("dede.jpg")
+#     with open('outputfile.json') as json_file:
+#         json_ob = json.load(json_file)  # read outputfile as json
+#     encoded_photo = json_ob["image"]
+    
+#     preds = model.predict(encoded_photo)
+#     dumped_preds = json.dumps({"preds": preds}, default=int32_to_int)
+#     print("Dumped preds: ", dumped_preds)    
+    
+    
+# if __name__ == "__main__":
+#     init_local()
