55ce4f40bc82c3b7ea99764be9621b786886d219
sglbl <silo1881@gmail.com>
2022-12-15 09:09:10 +0300

    Try7 no Pillow

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -2,9 +2,9 @@ import io
 import json
 import numpy as np
 import tensorflow as tf
-# import cv2 as cv
-from PIL import Image
-# import base64
+import cv2 as cv
+# from PIL import Image
+import base64
 import logging
 import azure.functions as func
 
