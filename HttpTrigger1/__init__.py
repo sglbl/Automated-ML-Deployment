d37b8a1e9267cc25ff784b90c1a97966901787ed
Suleyman <silo1881@gmail.com>
2022-12-15 19:31:30 +0300

    add libraries

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,9 +1,9 @@
-# import io
-# import json
-# import numpy as np
-# import tensorflow as tf
-# import cv2 as cv
-# from PIL import Image
+import io
+import json
+import numpy as np
+import tensorflow as tf
+import cv2 as cv
+from PIL import Image
 import base64
 import logging
 import azure.functions as func
